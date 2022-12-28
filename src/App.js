b4b65bd80a7cf6267d29adeb5af837b5b40cdb72
Amir Hamza <a.hamza8596@gmail.com>
2022-12-27 23:52:20 -0500

    Starting Authentication and Authorization

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 import LoginForm from './components/LoginForm';
 import SignUpForm from './components/SignUpForm';
 import NavBar from './components/NavBar';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  
   return (
     <div className="App">
       <LoginForm />
